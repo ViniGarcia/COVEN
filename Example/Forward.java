4718303a27974906afbd206eb64329d1569dd829
Vinícius F. Garcia <vfulber@inf.ufsm.br>
2019-04-01 11:44:01 -0300

    Java example updated

--- Example/Forward.java
+++ Example/Forward.java
@@ -8,14 +8,14 @@ public class Forward {
     try{
       Socket fromIR = (new ServerSocket(8008)).accept();
       Socket toIR = new Socket("localhost", 8009);
-      byte[] pktData = new byte[1550];
+      byte[] pktData = new byte[1514];
       int pktSize = 0;
 
       DataOutputStream pktSnd = new DataOutputStream(toIR.getOutputStream());
 
       while(true){
         pktSize = fromIR.getInputStream().read(pktData);
-        System.out.println("OK1!!");
+        System.out.println("OK1!! " + pktSize);
         pktSnd.write(Arrays.copyOfRange(pktData, 0, pktSize));
         pktSnd.flush();
       }
